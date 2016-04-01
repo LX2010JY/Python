51e1b8291d339dc41d528de189aba2e83cd18f08
luoxiao <2810752073@qq.com>
2016-04-01 18:20:58 +0800

    shiyanlou

--- my_Blog/article/views.py
+++ my_Blog/article/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from article.models import Article
 from django.http import HttpResponse
-
+from django.http import Http404
 # Create your views here.
 
 def home(request):
 	blog = Article.objects.all()
 	return render(request, 'home.html',{'blog_list':blog})
+
+def detail(request,id):
+	try:
+		post = Article.objects.get(id=str(id))
+	except Article.DoesNotExist:
+		raise Http404
+	return render(request, 'post.html',{'post':post})
